b4d2f80fd3264a5bc886e949a8e12d44b7c62065
Robert Lyndon Davis <robert.lyndon.davis@gmail.com>
2017-10-25 15:13:01 +0100

    - hooked up burger menu events

--- Quizzy/src/components/plugins/Navbar.vue
+++ Quizzy/src/components/plugins/Navbar.vue
@@ -3,25 +3,36 @@
       <div class="navbar-brand">
         <!-- Just using the vue logo here as i'm too lazy to make my own just yet -->
         <router-link class="navbar-item" :to="{ name: 'Landing' }"><img src="../../assets/logo.png"></router-link>
+
+        <a class="navbar-burger burger" v-bind:class="{'is-active' : burgerActive}" v-on:click="toggleBurger">
+          <span></span>
+          <span></span>
+          <span></span>
+        </a>
       </div>
-      <div class="navbar-menu">
+      <div class="navbar-menu" v-bind:class="{'is-active' : burgerActive}">
         <div class="navbar-start">
           <router-link v-show="!this.$root.appError.name" class="navbar-item" :to="{ name: 'NewGame' }">New Game</router-link>
           <router-link v-show="!this.$root.appError.name" class="navbar-item" :to="{ name: 'JoinGame' }">Join Game</router-link>
         </div>
       </div>
-      <!-- TODO: Hook up the events for the buger menu -->
-      <button class="button navbar-burger">
-        <span></span>
-        <span></span>
-        <span></span>
-      </button>
   </nav>
 </template>
 
 <script>
 export default {
-    name: 'Navbar'
+    name: 'Navbar',
+    data() {
+      return {
+        burgerActive: false
+      }
+    },
+
+    methods: {
+      toggleBurger() {
+        this.burgerActive = !this.burgerActive;
+      }
+    }
 }
 </script>
 
